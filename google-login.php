86e962a86e9d09e57c26a70731941121df3d603f
shez222 <bilal.shehroz420@gmail.com>
2025-01-25 03:07:31 +0500

    push

--- google-login.php
+++ google-login.php
@@ -6,13 +6,16 @@ $client = new Google_Client();
 $client->setClientId('599470363677-igrb9daksarvf22aj4bfd59ugbia12dd.apps.googleusercontent.com');
 $client->setClientSecret('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');
 $client->setRedirectUri('https://fennec.digicomet.net/google-login.php');
-$client->addScope('email');
-$client->addScope('profile');
+$client->addScope(['email', 'profile']);
+
 if (isset($_GET['code'])) {
     $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    if (isset($token['error'])) {
+        // Handle error
+        exit('Error fetching access token');
+    }
 
     $client->setAccessToken($token['access_token']);
-
     $google_service = new Google_Service_Oauth2($client);
     $user_info = $google_service->userinfo->get();
 
@@ -23,23 +26,19 @@ if (isset($_GET['code'])) {
     $user = $dbFunctions->getDatanotenc('users', $where);
 
     if ($user) {
+        // Existing user, set session and redirect
         $user = $user[0];
         $sessionSet = $fun->sessionSet($email);
+        header("Location: index.php");
+        exit();
     } else {
-        $data = [
+        // New user, store temp data and redirect to role selection
+        $_SESSION['temp_user'] = [
             'email' => $email,
-            'username' => $name,
-            'password' => password_hash(bin2hex(random_bytes(10)), PASSWORD_DEFAULT),
-            'created_at' => date('Y-m-d H:i:s'),
-            'role' => 0,
-            'verification_token' => 0,
-            'email_verified_at' => date('Y-m-d H:i:s')
+            'name' => $name
         ];
-        $dbFunctions->setData('users', $data);
-        $sessionSet = $fun->sessionSet($email);
+        header("Location: select-role.php");
+        exit();
     }
-    // Server-side redirection
-    header("Location: index.php");
-    exit();
 }
 ?>
