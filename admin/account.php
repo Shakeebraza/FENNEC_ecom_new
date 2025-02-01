296c1549739dea655ef4ae7f64b8819a6367aa04
Muhammad Shakeeb Raza <shakeebraza90@gmail.com>
2025-02-01 17:58:30 -0500

    fix:"chnages can be done"

--- admin/account.php
+++ admin/account.php
@@ -2,11 +2,11 @@
 require_once("../global.php");
 include_once('header.php');
 
-$username = $_SESSION['username'] ?? ''; 
-$formemail = $_SESSION['email'] ?? ''; 
-$userid = intval(base64_decode($_SESSION['userid'])) ?? 0; 
+$username = $_SESSION['ausername'] ?? ''; 
+$formemail = $_SESSION['aemail'] ?? ''; 
+$userid = intval(base64_decode($_SESSION['auserid'])) ?? 0; 
 $userData = $dbFunctions->getDatanotenc('user_detail', "userid = '$userid'");
-$profileImage = $_SESSION['profile'] === '' ? $urlval . 'images/profile.jpg' : $_SESSION['profile'];
+$profileImage = $_SESSION['aprofile'] === '' ? $urlval . 'images/profile.jpg' : $_SESSION['profile'];
 $csrfError = '';
 $successMessage = '';
 
@@ -48,8 +48,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         ];
                         $chnggg=$dbFunctions->updateData('users', $userData, $userid);
                         var_dump($chnggg);
-                        $_SESSION['profile'] = $urlval.$databaseProfilePath;
-                        $_SESSION['username'] = $newUsername;
+                        $_SESSION['aprofile'] = $urlval.$databaseProfilePath;
+                        $_SESSION['ausername'] = $newUsername;
                     } else {
                         $csrfError = "Failed to upload profile image.";
                     }
@@ -59,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                     ];
                     $dbFunctions->updateData('users', $userData, $userid);
-                    $_SESSION['username'] = $newUsername;
+                    $_SESSION['ausername'] = $newUsername;
                 }
                 if (empty($newNumber) || empty($newAddress) || empty($newCountry) || empty($newCity)) {
                     $csrfError = "Please fill all the fields!";
@@ -84,7 +84,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         $successMessage = "Account information inserted successfully!";
                     }
 
-                    $_SESSION['username'] = $newUsername;
+                    $_SESSION['ausername'] = $newUsername;
                 }
             }
         }
@@ -110,18 +110,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             </div>
                             <div class="card-body">
                                 <?php if ($csrfError): ?>
-                                    <div class="alert alert-danger"><?php echo $csrfError; ?></div>
+                                <div class="alert alert-danger"><?php echo $csrfError; ?></div>
                                 <?php elseif ($successMessage): ?>
-                                    <div class="alert alert-success"><?php echo $successMessage; ?></div>
+                                <div class="alert alert-success"><?php echo $successMessage; ?></div>
                                 <?php endif; ?>
 
                                 <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
                                     <div class="row form-group">
                                         <div class="col text-center">
-                                            <img src="<?php echo htmlspecialchars($profileImage); ?>" alt="Profile Image" width="150px" class="rounded-circle mb-3">
+                                            <img src="<?php echo htmlspecialchars($profileImage); ?>"
+                                                alt="Profile Image" width="150px" class="rounded-circle mb-3">
                                             <div class="custom-file mb-3">
-                                                <input type="file" class="custom-file-input" id="profile-image" name="profile_image">
-                                                <label class="custom-file-label" for="profile-image">Choose Profile Image</label>
+                                                <input type="file" class="custom-file-input" id="profile-image"
+                                                    name="profile_image">
+                                                <label class="custom-file-label" for="profile-image">Choose Profile
+                                                    Image</label>
                                             </div>
                                         </div>
                                     </div>
@@ -129,43 +132,57 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                     <div class="row form-group">
                                         <div class="col">
                                             <label for="username" class="form-label">Username</label>
-                                            <input type="text" id="username" name="username" value="<?php echo htmlspecialchars($username); ?>" class="form-control form-control-lg " placeholder="Enter your username">
+                                            <input type="text" id="username" name="username"
+                                                value="<?php echo htmlspecialchars($username); ?>"
+                                                class="form-control form-control-lg " placeholder="Enter your username">
                                         </div>
                                     </div>
 
                                     <div class="row form-group">
                                         <div class="col">
                                             <label for="email" class="form-label">Email</label>
-                                            <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($formemail); ?>" class="form-control form-control-lg " readonly>
+                                            <input type="email" id="email" name="email"
+                                                value="<?php echo htmlspecialchars($formemail); ?>"
+                                                class="form-control form-control-lg " readonly>
                                         </div>
                                     </div>
 
                                     <div class="row form-group">
                                         <div class="col">
                                             <label for="number" class="form-label">Phone Number</label>
-                                            <input type="text" id="number" name="number" value="<?php echo isset($userData[0]['number']) ? htmlspecialchars($userData[0]['number']) : ''; ?>" class="form-control form-control-lg " placeholder="Enter your phone number">
+                                            <input type="text" id="number" name="number"
+                                                value="<?php echo isset($userData[0]['number']) ? htmlspecialchars($userData[0]['number']) : ''; ?>"
+                                                class="form-control form-control-lg "
+                                                placeholder="Enter your phone number">
                                         </div>
                                     </div>
 
                                     <div class="row form-group">
                                         <div class="col">
                                             <label for="address" class="form-label">Address</label>
-                                            <input type="text" id="address" name="address" value="<?php echo isset($userData[0]['address']) ? htmlspecialchars($userData[0]['address']) : ''; ?>" class="form-control form-control-lg " placeholder="Enter your address">
+                                            <input type="text" id="address" name="address"
+                                                value="<?php echo isset($userData[0]['address']) ? htmlspecialchars($userData[0]['address']) : ''; ?>"
+                                                class="form-control form-control-lg " placeholder="Enter your address">
                                         </div>
                                     </div>
                                     <div class="row form-group">
                                         <div class="col">
                                             <label for="country" class="form-label">Country</label>
-                                            <input type="text" id="country" name="country" value="<?php echo isset($userData[0]['country']) ? htmlspecialchars($userData[0]['country']) : ''; ?>" class="form-control form-control-lg " placeholder="Enter your country">
+                                            <input type="text" id="country" name="country"
+                                                value="<?php echo isset($userData[0]['country']) ? htmlspecialchars($userData[0]['country']) : ''; ?>"
+                                                class="form-control form-control-lg " placeholder="Enter your country">
                                         </div>
                                     </div>
                                     <div class="row form-group">
                                         <div class="col">
                                             <label for="city" class="form-label">City</label>
-                                            <input type="text" id="city" name="city" value="<?php echo isset($userData[0]['city']) ? htmlspecialchars($userData[0]['city']) : ''; ?>" class="form-control form-control-lg " placeholder="Enter your city">
+                                            <input type="text" id="city" name="city"
+                                                value="<?php echo isset($userData[0]['city']) ? htmlspecialchars($userData[0]['city']) : ''; ?>"
+                                                class="form-control form-control-lg " placeholder="Enter your city">
                                         </div>
                                     </div>
-                                    <input type="hidden" name="csrf_token" id="csrf_token" value="<?php echo $CsrfProtection->generateToken(); ?>">
+                                    <input type="hidden" name="csrf_token" id="csrf_token"
+                                        value="<?php echo $CsrfProtection->generateToken(); ?>">
                                     <div class="row form-group">
                                         <div class="col text-center">
                                             <button type="submit" class="btn btn-success btn-lg">Update Account</button>
@@ -175,7 +192,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             </div>
                         </div>
                     </div>
-                </div> 
+                </div>
             </div>
         </div>
     </div>
